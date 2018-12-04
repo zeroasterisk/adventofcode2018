a8f423c5c290cf16d120ea809a9427e907b80a86
alan bount <alan@zeroasterisk.com>
2018-12-03 22:01:40 -0700

    init sleepy

---
+++ 04/sleepy/lib/sleepy.ex
@@ -0,0 +1,18 @@
+defmodule Sleepy do
+  @moduledoc """
+  Documentation for Sleepy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sleepy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 04/sleepy/test/sleepy_test.exs
@@ -0,0 +1,8 @@
+defmodule SleepyTest do
+  use ExUnit.Case
+  doctest Sleepy
+
+  test "greets the world" do
+    assert Sleepy.hello() == :world
+  end
+end
